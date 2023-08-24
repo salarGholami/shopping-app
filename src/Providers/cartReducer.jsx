d3a83b26e820bf05c12b1fd98606f954eaac5e97
Salar Gholami <salargholami021@gmail.com>
2023-08-24 21:44:53 +0330

    add badge to navigation

--- src/Providers/cartReducer.jsx
+++ src/Providers/cartReducer.jsx
@@ -1,54 +1,58 @@
+const addProductTocart = (state, payload) => {
+  const updatedCart = [...state.cart];
+
+  const updatedItemIndex = updatedCart.findIndex(
+    (item) => item.id === payload.id
+  );
+
+  if (updatedItemIndex < 0) {
+    updatedCart.push({ ...payload, quantity: 1 });
+  } else {
+    const updatedItem = { ...updatedCart[updatedItemIndex] };
+    updatedItem.quantity++;
+    updatedCart[updatedItemIndex] = updatedItem;
+  }
+
+  return {
+    ...state,
+    cart: updatedCart,
+    total: state.total + payload.price,
+  };
+};
+
+const removeProducrFromCart = (state, payload) => {
+  const updatedCart = [...state.cart];
+
+  const updatedItemIndex = updatedCart.findIndex(
+    (item) => item.id === payload.id
+  );
+  const updatedItem = { ...updatedCart[updatedItemIndex] };
+
+  if (updatedItem.quantity === 1) {
+    const filterCart = updatedCart.filter((item) => item.id !== payload.id);
+    return {
+      ...state, 
+      cart: filterCart,
+      total: state.total - payload.price,
+    };
+  } else {
+    updatedItem.quantity--;
+    updatedCart[updatedItemIndex] = updatedItem;
+    return {
+      ...state,
+      cart: updatedCart,
+      total: state.total - payload.price,
+    };
+  }
+};
+
 const cartReducer = (state, action) => {
   switch (action.type) {
-    case "ADD_TO_CART": {
-      const upatedCart = [...state.cart];
+    case "ADD_TO_CART":
+      return addProductTocart(state, action.payload);
 
-      const updatedItemIndex = upatedCart.findIndex(
-        (item) => item.id === action.payload.id
-      );
-
-      if (updatedItemIndex < 0) {
-        upatedCart.push({ ...action.payload, quantity: 1 });
-      } else {
-        const updatedItem = { ...upatedCart[updatedItemIndex] };
-        updatedItem.quantity++;
-        upatedCart[updatedItemIndex] = updatedItem;
-      }
-
-      return {
-        ...state,
-        cart: upatedCart,
-        total: state.total + action.payload.price,
-      };
-    }
-
-    case "REMOVE-_PRODUCT": {
-      const updatedCart = [...state.cart];
-
-      const updatedItemIndex = updatedCart.findIndex(
-        (item) => item.id === action.payload.id
-      );
-      const updatedItem = { ...updatedCart[updatedItemIndex] };
-
-      if (updatedItem.quantity === 1) {
-        const filterCart = updatedCart.filter(
-          (item) => item.id !== action.payload.id
-        );
-        return {
-          ...state,
-          cart: filterCart,
-          total: state.total - action.payload.price,
-        };
-      } else {
-        updatedItem.quantity--;
-        updatedCart[updatedItemIndex] = updatedItem;
-        return {
-          ...state,
-          cart: updatedCart,
-          total: state.total - action.payload.price,
-        };
-      }
-    }
+    case "REMOVE-_PRODUCT":
+      return removeProducrFromCart(state, action.payload);
     default:
       return state;
   }
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,7 +1,9 @@
 import { NavLink } from "react-router-dom";
 import style from "./Navigation.module.css";
+import { useCart } from "../../Providers/CartPeovider";
 
 const Navigation = () => {
+  const { cart } = useCart();
   return (
     <div>
       <header className={style.mainNavigation}>
@@ -10,8 +12,11 @@ const Navigation = () => {
             <li>
               <NavLink to="/">home</NavLink>
             </li>
-            <li>
-              <NavLink to="/cart">cart</NavLink>
+            <li className={style.cartLink}>
+              <NavLink to="/cart">
+                cart
+                <span>{cart.length}</span>
+              </NavLink>
             </li>
           </ul>
           <div>slm salar</div>
